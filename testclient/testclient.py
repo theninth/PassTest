46bb198f5247b899f9592e3087cab2ffa1ae661f
Niclas Nilsson <thenajnth@gmail.com>
2013-08-09 22:47:40 +0200

    Added experimental GUI for testclient

--- testclient/testclient.py
+++ testclient/testclient.py
@@ -1,13 +1,67 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import argparse
+import wx
 from getpass import getpass
 from passtest import Hashes
 
 # Create main object for passtest
 hashes = Hashes()
 
+# GUI Stuff
+class MainFrame(wx.Frame):
+    """Handles a GUI password tester"""
+    def __init__(self, name, *args, **kwds):
+        kwds["style"] = wx.DEFAULT_FRAME_STYLE
+        wx.Frame.__init__(self, *args, **kwds)
+        self.label = wx.StaticText(self, -1, "")
+        self.panel = wx.Panel(self, -1)
+        self.ok_button = wx.Button(self, -1, "OK")
+
+        # Build layout and set properties of frame
+        self.SetTitle("PassTest")
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.label, 0, wx.EXPAND, 0)
+        sizer.Add(self.panel, 1, wx.EXPAND, 0)
+        sizer.Add(self.ok_button, 0, wx.EXPAND, 0)
+        self.SetSizer(sizer)
+        sizer.Fit(self)
+        self.Layout()
+
+        # Bindings
+        self.Bind(wx.EVT_BUTTON, self.OnExitApp, self.ok_button)
+        self.Bind(wx.EVT_CLOSE, self.OnExitApp)
+
+        # Password dialog
+        box = wx.PasswordEntryDialog(None, 'Please type the password for: ' + name,
+                                 'Password')
+        
+        # On Password box OK
+        if box.ShowModal() == wx.ID_OK:
+            passwd = box.GetValue()
+            h = hashes.get(name)
+            
+            # Correct password
+            if h.test_passwd(passwd):
+                self.label.SetLabel('Correct!')
+            # Incorrect password
+            else:
+                self.label.SetLabel('Incorrect!')
+            box.Destroy()
+        
+        # On Password box cancel
+        else:
+            self.Destroy()
+            sys.exit(0)
+
+    def OnExitApp(self, event):  
+        self.Destroy()
+        sys.exit(0)
+
+
+# Generic functions
 def test_passwd(name):
     passwd = getpass()
     h = hashes.get(name)
@@ -15,6 +69,12 @@ def test_passwd(name):
         print 'Yeah! That was the right password baby'
     else:
         print 'Sorry, that was not the right password!'
+
+def gui_test_passwd(name):
+    app = wx.App(0)
+    frame = MainFrame(parent=None, id=-1)
+    frame.Show()
+    app.MainLoop()
 
 def add_passwd(name):
     while True:
@@ -34,6 +94,7 @@ def main():
     parser.add_argument('-n','--name', help='Identifier for password')
     parser.add_argument('-l','--list', help='List passwd id:s', action='store_true')
     parser.add_argument('-t', '--test', help='Tests if password matches hash', action='store_true')
+    parser.add_argument('-g', '--guitest', help='GUI Test if password matches hash - EXPERIMENTAL', action='store_true')
 
     args = vars(parser.parse_args())
 
@@ -51,6 +112,17 @@ def main():
                 print 'Hash not found!'
         else:
             print 'This option is only available with the --name argument'
+
+    # run gui test argument
+    elif args['guitest']:
+        if args['name']:
+            app = wx.App(0)
+            frame = MainFrame(args['name'], parent=None, id=-1)
+            frame.Show()
+            app.MainLoop()
+        else:
+            print 'This option is only available with the --name argument'
+
 
     # run add argument
     elif args['add']:
